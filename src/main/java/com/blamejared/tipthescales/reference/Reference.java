11a191829dff7766959a30b84e5360d8a302b288
Jared <jaredlll08@gmail.com>
2018-12-21 22:29:54 +0200

    version push

--- src/main/java/com/blamejared/tipthescales/reference/Reference.java
+++ src/main/java/com/blamejared/tipthescales/reference/Reference.java
@@ -4,6 +4,6 @@ public class Reference {
     
     public static final String MODID = "tipthescales";
     public static final String NAME = "TipTheScales";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
     
 }
