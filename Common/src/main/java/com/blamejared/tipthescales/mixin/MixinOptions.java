37005583193ceee6ffafac97d15e3513fca80a98
Jared <jaredlll08@gmail.com>
2022-09-01 19:55:56 +0200

    Fixed guiscale always reverting to auto

--- Common/src/main/java/com/blamejared/tipthescales/mixin/MixinOptions.java
+++ Common/src/main/java/com/blamejared/tipthescales/mixin/MixinOptions.java
@@ -4,26 +4,21 @@ import com.blamejared.tipthescales.ClampingLazyMaxIntRangeSlider;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.OptionInstance;
 import net.minecraft.client.Options;
-import net.minecraft.client.main.GameConfig;
 import net.minecraft.network.chat.Component;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Minecraft.class)
-public class MixinMinecraft {
+import java.io.File;
+
+@Mixin(Options.class)
+public class MixinOptions {
     
-    @Shadow
-    @Final
-    public Options options;
-    
-    @Inject(method = "<init>", at = @At(value = "TAIL"))
-    public void tipthescales$init(GameConfig $$0, CallbackInfo ci) {
+    @Inject(method = "load", at = @At(value = "HEAD"))
+    public void tipthescales$load(CallbackInfo ci) {
         
-        ((AccessOptions) this.options).tipthescales$setGuiScale(new OptionInstance<>("options.guiScale", OptionInstance.noTooltip(), (caption, value) -> {
+        ((AccessOptions) this).tipthescales$setGuiScale(new OptionInstance<>("options.guiScale", OptionInstance.noTooltip(), (caption, value) -> {
             Component current = value == 0 ? Component.translatable("options.guiScale.auto") : Component.literal(Integer.toString(value));
             return Options.genericValueLabel(caption, current);
         }, new ClampingLazyMaxIntRangeSlider(0, () -> {
