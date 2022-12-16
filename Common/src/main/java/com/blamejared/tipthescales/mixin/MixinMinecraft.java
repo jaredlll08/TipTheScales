ebb7c8e64f6aa21177442775935c711742203485
Jared <jaredlll08@gmail.com>
2022-12-15 19:11:59 -0700

    Fix crash due to AT's not being applied sometimes. Close #23 Close #24

--- Common/src/main/java/com/blamejared/tipthescales/mixin/MixinMinecraft.java
+++ Common/src/main/java/com/blamejared/tipthescales/mixin/MixinMinecraft.java
@@ -4,26 +4,30 @@ import com.blamejared.tipthescales.ClampingLazyMaxIntRangeSlider;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.OptionInstance;
 import net.minecraft.client.Options;
+import net.minecraft.client.main.GameConfig;
 import net.minecraft.network.chat.Component;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.io.File;
-
-@Mixin(Options.class)
-public class MixinOptions {
+@Mixin(Minecraft.class)
+public class MixinMinecraft {
     
-    @Inject(method = "load", at = @At(value = "HEAD"))
-    public void tipthescales$load(CallbackInfo ci) {
-        
-        ((AccessOptions) this).tipthescales$setGuiScale(new OptionInstance<>("options.guiScale", OptionInstance.noTooltip(), (caption, value) -> {
+    @Shadow
+    @Final
+    public Options options;
+    
+    @Inject(method = "<init>", at = @At("TAIL"))
+    public void init(GameConfig gameConfig, CallbackInfo ci) {
+        ((AccessOptions) this.options).tipthescales$setGuiScale(new OptionInstance<>("options.guiScale", OptionInstance.noTooltip(), (caption, value) -> {
             Component current = value == 0 ? Component.translatable("options.guiScale.auto") : Component.literal(Integer.toString(value));
             return Options.genericValueLabel(caption, current);
         }, new ClampingLazyMaxIntRangeSlider(0, () -> {
-            Minecraft $$0x = Minecraft.getInstance();
-            return !$$0x.isRunning() ? 2147483646 : $$0x.getWindow().calculateScale(0, $$0x.isEnforceUnicode());
+            Minecraft mc = Minecraft.getInstance();
+            return !mc.isRunning() ? 2147483646 : mc.getWindow().calculateScale(0, mc.isEnforceUnicode());
         }), 0, (value) -> {
         }));
     }
