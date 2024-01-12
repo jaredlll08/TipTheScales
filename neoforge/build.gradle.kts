76a6626bdafb38e0c6ad49b26d456031ef1b15d3
Jared <jaredlll08@gmail.com>
2024-01-11 20:30:35 -0700

    Port to 1.20.4

--- neoforge/build.gradle.kts
+++ neoforge/build.gradle.kts
@@ -7,7 +7,7 @@ import net.darkhax.curseforgegradle.TaskPublishCurseForge
 plugins {
     id("com.blamejared.tipthescales.default")
     id("com.blamejared.tipthescales.loader")
-    id("net.neoforged.gradle.userdev") version ("7.0.71")
+    id("net.neoforged.gradle.userdev") version ("7.0.81")
     id("com.modrinth.minotaur")
 }
 
