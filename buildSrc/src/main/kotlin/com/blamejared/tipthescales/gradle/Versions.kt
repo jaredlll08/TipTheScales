02c7937ae7b1a0c9ef80f0c36387198421ceae9c
Jared <jaredlll08@gmail.com>
2024-01-11 20:35:52 -0700

    Update mod version

--- buildSrc/src/main/kotlin/com/blamejared/tipthescales/gradle/Versions.kt
+++ buildSrc/src/main/kotlin/com/blamejared/tipthescales/gradle/Versions.kt
@@ -1,7 +1,7 @@
 package com.blamejared.tipthescales.gradle
 
 object Versions {
-    const val MOD = "11.0.0"
+    const val MOD = "12.0.0"
     const val JAVA = "17"
     const val MINECRAFT = "1.20.4"
     const val FABRIC_LOADER = "0.15.3"
